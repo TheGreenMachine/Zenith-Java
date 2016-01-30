2cb71695499337c38b40b8d536f5ca07da2744b1
ElliottOlson <elliotto17258@apps.edina.k12.mn.us>
2016-01-30 11:37:38 -0600

    Updated Drivetrain subsystem.

--- src/com/edinarobotics/zenith/Components.java
+++ src/com/edinarobotics/zenith/Components.java
@@ -14,10 +14,12 @@ public class Components {
 
 	// CAN Constants
 		// Drivetrain Constants
-		private static final int TOP_LEFT_CANTALON = 1;
-		private static final int TOP_RIGHT_CANTALON = 2;
-		private static final int BOTTOM_LEFT_CANTALON = 3;
-		private static final int BOTTOM_RIGHT_CANTALON = 4;
+		private static final int TOP_LEFT_CANTALON = 3;
+		private static final int TOP_RIGHT_CANTALON = 7;
+		private static final int MIDDLE_LEFT_CANTALON = 2;
+		private static final int MIDDLE_RIGHT_CANTALON = 6;
+		private static final int BOTTOM_LEFT_CANTALON = 1;
+		private static final int BOTTOM_RIGHT_CANTALON = 5;
 		// End Drivetrain Constants
 		
 		//Potentiometer Constants
@@ -27,14 +29,15 @@ public class Components {
 		//End Potentiometer Constants
 		
 		//Claw Constants
-		private static final int CLAW_CANTALON = 5;
+		private static final int CLAW_CANTALON = 4;
 		//End Claw Constants
 		
 	// End CAN Constants
 
 	private Components() {
 		drivetrain = new Drivetrain(TOP_LEFT_CANTALON, TOP_RIGHT_CANTALON, 
-				BOTTOM_LEFT_CANTALON, BOTTOM_RIGHT_CANTALON);
+				MIDDLE_LEFT_CANTALON, MIDDLE_RIGHT_CANTALON, BOTTOM_LEFT_CANTALON, 
+				BOTTOM_RIGHT_CANTALON);
 		potentiometer = new AnalogPotentiometer(POTENTIOMETER_CHANNEL, 
 				POTENTIOMETER_RANGE, POTENTIOMETER_OFFSET);
 		claw = new Claw(CLAW_CANTALON);
--- src/com/edinarobotics/zenith/subsystems/Drivetrain.java
+++ src/com/edinarobotics/zenith/subsystems/Drivetrain.java
@@ -4,25 +4,35 @@ import com.edinarobotics.utils.subsystems.Subsystem1816;
 
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.CANTalon.TalonControlMode;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class Drivetrain extends Subsystem1816 {
 
 	private RobotDrive robotDrive;
-	private CANTalon topLeft, topRight, bottomLeft, bottomRight;
+	private CANTalon topLeft, topRight, middleLeft, middleRight, bottomLeft, bottomRight;
 	private double verticalStrafe, rotation;
 
 	private boolean lowGear;
 	private final double LOW_GEAR_SPEED = 0.75;
 
-	public Drivetrain(int topLeft, int topRight, int bottomLeft, int bottomRight) {
+	public Drivetrain(int topLeft, int topRight, int middleLeft, int middleRight, 
+			int bottomLeft, int bottomRight) {
 		this.topLeft = new CANTalon(topLeft);
 		this.topRight = new CANTalon(topRight);
+		this.middleLeft = new CANTalon(middleLeft);
+		this.middleRight = new CANTalon(middleRight);
 		this.bottomLeft = new CANTalon(bottomLeft);
 		this.bottomRight = new CANTalon(bottomRight);
 
 		robotDrive = new RobotDrive(this.topLeft, this.bottomLeft, this.topRight, 
 				this.bottomRight);
+		
+		this.middleLeft.changeControlMode(TalonControlMode.Follower);
+		this.middleRight.changeControlMode(TalonControlMode.Follower);
+		
+		this.middleLeft.set(3);
+		this.middleRight.set(7);
 	}
 
 	@Override
